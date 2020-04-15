27f4163fc4c3476189a69d62da0dd6a37fd601ac
yjoung-7874 <people7874@gmail.com>
2020-04-16 05:21:07 +0900

    update

--- include/dof4_robot_arm/HerkuleX.h
+++ include/dof4_robot_arm/HerkuleX.h
@@ -40,7 +40,7 @@ using namespace std;
 #define CHKSUM_MASK                         0xFE
 
 #define LSB(data) ((unsigned char)(data))
-#define MSB(data) ((unsigined char)((unsigned int)(data)>>8)&0xFF)
+#define MSB(data) ((unsigned char)((unsigned int)(data)>>8)&0xFF)
 
 // LED
 #define HERKULEX_LED_RED	0x10
@@ -64,33 +64,34 @@ public :
 
 class HerkuleX {
 public:
-    HerkuleX();	
-    ~HerkuleX();
+    HerkuleX();			//생성자 : USB2Serial 연결
+    ~HerkuleX();		//소멸자 : USB2Serial 연결 해제
 
     //void setAckPolicy(int id, bool value);	//응답 정책 설정
     //void setLEDPolicy(int id, bool value);	//LED 점멸 정책 설정
+
     void clear(int id);				//에러 삭제
 
     void TorqueOn(int id);		//torque on 
     void TorqueOff(int id);		//torque off
 
-    void turn(int id, int speed, int playtime=60, int led=0);	//Motor Turn 속도 제어
-    float getTurnSpeed(int id);     				//turn 속도  position변화량/11.2ms 		
-
-    void movePos(int id, int pos, int playtime=60, int led=0);	//Motor의 Position 제어
+    void turn(int id, int speed, int playtime=60, int led=0);    //Motor Turn 속도 제어
+    float getTurnSpeed(int id);                                  //turn 속도  position변화량/11.2ms
+    void movePos(int id, int pos, int playtime=60, int led=0);	 //Motor의 Position 제어
     void movePos(map<int, int> motor_values, int playtime=60, int led=0);
-    int getPos(int id);						//Motor의 Position 값 리턴
+    int getPos(int id);                                          //Motor의 Position 값 리턴
 
-    void moveAngle(int id, float angle, int playtime=60, int led=0);	//Motor의 각도 제어
-    void moveAngle(map<int, float> motor_angles, int playtime, int led);
-    float getAngle(int id);						//Motor의 각도 값 리턴
+    void moveAngle(int id, float angle, int playtime=60, int led=0); // Motor의 각도 제어
+    void moveAngle(map<int, float> motor_angles, int playtime=60, int led=0);
+
+    float getAngle(int id);	                                         // Motor의 각도 값 리턴
 
     void setLed(int id, int led);			//Motor LED
     void reboot(int id);				//Motor
 
 private:
     bool Open();		//serial port open
-    bool Close();		//serial port close
+    bool Close();
 
     unsigned char getChksum1(class DataPacket * buf);	//Check sum1
     unsigned char getChksum2(unsigned char chksum1);	//Check sum2
